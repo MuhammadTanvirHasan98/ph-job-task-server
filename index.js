d3531d405737cb8d6dcf6534acc19e94ac28cf31
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2024-08-18 22:29:22 +0600

    Mongodb setup done and get all foods data from database.

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
-
 const express = require('express')
-const app = express()
+
+const app = express();
 const port = process.env.PORT || 3000
+
+// using middleware
+app.use(cors());
+
+
+
+
+const uri = "mongodb+srv://huntUser:muhammad98@muhammadcluster.h7migjc.mongodb.net/?retryWrites=true&w=majority&appName=MuhammadCluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+     const db = client.db("huntFoodsDB");
+    const allFoodCollection = db.collection("allFoods") 
+
+
+    app.get("/allFoods", async(req, res)=>{
+      
+      const result = await allFoodCollection.find().toArray();
+      if (result.length === 0) {
+        return res.status(204).send();
+      }
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
